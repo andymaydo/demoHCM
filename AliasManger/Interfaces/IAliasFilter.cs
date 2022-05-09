5e0eadaa67a30e55633d7ae5e649d2b7fb5d39f8
Kirkor Maydozian <maydozian@csdevelop.onmicrosoft.com>
2022-05-09 18:26:54 +0300

    1. Alias Manger as a local project copy

---
+++ AliasManger/Interfaces/IAliasFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliasManger.Interfaces
+{
+    internal interface IAliasFilter
+    {
+        public string HcmProfileId { get; set; }
+        public string VgsLicenseId { get; set; }
+        public string CloudAppId { get; set; }
+        public int StatusId { get; set; }
+
+        public string Name { get; set; }
+        public string Street { get; set; }
+
+        public bool WaitAuth { get; set; }
+        public DateTime Von { get; set; }
+        public DateTime Bis { get; set; }
+        public bool AllTime { get; set; }
+    }
+}
