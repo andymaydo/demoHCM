e71f389016533b2168502faade962059fbc2a66b
Kirkor Maydozian <maydozian@csdevelop.onmicrosoft.com>
2022-07-11 17:24:27 +0300

    CryptDecrypt HCM Lic Data

--- VgsRepo/LicLimits.cs
+++ VgsRepo/LicLimits.cs
@@ -9,15 +9,18 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CryptDecryptLib;
 
 namespace VgsRepo
 {
     public class LicLimits : ILicLimits
     {
         private string _sqlConnStr;
+        private CryptDecrypt _cryptDecrypt;
         public LicLimits(IConfiguration config)
         {
             _sqlConnStr = config.GetConnectionString("VGS");
+            _cryptDecrypt = new CryptDecrypt("PO9$&!VCzae*%?<,[$");
         }
 
         public async Task<int> GetUserCountLimit(string applianceId)
@@ -32,9 +35,11 @@ namespace VgsRepo
             {
                 await conn.ExecuteAsync(sql, p, commandType: CommandType.StoredProcedure);
 
-                var usrCnt = p.Get<String>("@UserCountLimit");
+                var usrCntCrypted = p.Get<String>("@UserCountLimit");
 
-                return Int32.Parse(usrCnt);
+                var usrCntDecrypted = _cryptDecrypt.DecryptData(usrCntCrypted);
+
+                return Int32.Parse(usrCntDecrypted);
             }
 
         }
@@ -51,19 +56,23 @@ namespace VgsRepo
             {
                 await conn.ExecuteAsync(sql, p, commandType: CommandType.StoredProcedure);
 
-                var prfCnt = p.Get<String>("@ProfileCountLimit");
+                var prfCntCrypted = p.Get<String>("@ProfileCountLimit");
 
-                return Int32.Parse(prfCnt);
+                var prfCntDecrypted = _cryptDecrypt.DecryptData(prfCntCrypted);
+
+                return Int32.Parse(prfCntDecrypted);
             }
         }
 
 
         public async Task SetCurrentUserCount(string applianceId, int currUserCount)
         {
+            var usrCntCrypted = _cryptDecrypt.EncryptData(currUserCount.ToString());
+
             string sql = "[dbo].[pHCMLicense_CurrentUsrCnt_Update]";
 
             var p = new DynamicParameters();
-            p.Add("@UserCountCurrent", currUserCount);
+            p.Add("@UserCountCurrent", usrCntCrypted);
 
             using (var conn = new SqlConnection(_sqlConnStr))
             {
@@ -74,10 +83,12 @@ namespace VgsRepo
 
         public async Task SetCurrentProfileCount(string applianceId, int currProfileCount)
         {
+            var prfCntCrypted = _cryptDecrypt.EncryptData(currProfileCount.ToString());
+
             string sql = "[dbo].[pHCMLicense_CurrentProfileCnt_Update]";
 
             var p = new DynamicParameters();            
-            p.Add("@ProfileCountCurrent", currProfileCount);
+            p.Add("@ProfileCountCurrent", prfCntCrypted);
 
             using (var conn = new SqlConnection(_sqlConnStr))
             {
